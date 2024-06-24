aa9132505fde1c983ab384bbb11ff27c125acfb0
sjovanovic974 <sjovanovic974@gmail.com>
2024-06-24 17:27:32 +0200

    Completed frontend

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const ideasRouter = require('./routes/ideas');
 const connectDB = require('./config/db');
 require('dotenv').config();
@@ -8,9 +10,20 @@ connectDB();
 
 const app = express();
 
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Cors middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
 app.use('/api/ideas', ideasRouter);
 
